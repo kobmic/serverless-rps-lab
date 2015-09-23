722730ed67dc8dad669fe6c7c644014105882bfc
Michael Kober <michael.kober@jayway.com>
2015-09-23 21:08:11 +0200

    fixed hello world example

---
+++ hello-lambda/src/main/java/com/jayway/rps/HelloLambda.java
@@ -0,0 +1,10 @@
+package com.jayway.rps;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class HelloLambda {
+
+   // your code goes here
+    
+}
