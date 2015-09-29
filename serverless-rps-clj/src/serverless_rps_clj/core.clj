7721a34f5a7cbbb485aa3e425a1f6825febcb19e
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2015-09-29 16:34:46 +0200

    Converted rest of functions to use deflambda.

--- serverless-rps-clj/src/serverless_rps_clj/core.clj
+++ serverless-rps-clj/src/serverless_rps_clj/core.clj
@@ -22,7 +22,27 @@
 
 ;; convenience macro for generating gen-class and handleRequest
 
-(defmacro deflambda [name]
+(defmacro deflambda
+  "Event handler that you write:
+  
+    (defn handle-create-game-event [event]
+      (prn event)
+      (game-store/create-game (str (UUID/randomUUID)) (:email event)))
+  
+  Then add a call to deflambda:
+  
+    (deflambda create-game)
+  
+  The deflambda macro generates this code:
+  
+    (gen-class
+      :name CreateGame
+      :prefix CreateGame-
+      :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
+    
+    (defn CreateGame-handleRequest [this is os context]
+      (handleRequest this is os context handle-create-game-event))"
+  [name]
   (let [class-name (->> (clojure.string/split (str name) #"-")
                      (mapcat clojure.string/capitalize)
                      (apply str))]
@@ -42,74 +62,34 @@
 
 (deflambda create-game)
 
-(comment
-
-  ;; event handler that you write
-  (defn handle-create-game-event [event]
-    (prn event)
-    (game-store/create-game (str (UUID/randomUUID)) (:email event)))
-
-  ;; the deflambda macro generates this code
-  (gen-class
-    :name CreateGame
-    :prefix CreateGame-
-    :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
-
-  (defn CreateGame-handleRequest [this is os context]
-    (handleRequest this is os context handle-create-game-event)))
-
 ;; Get Game
-
-(gen-class
-  :name GetGame
-  :prefix GetGame-
-  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
 
 (defn handle-get-game-event [event]
   (prn event)
   (game-store/get-game (:game-id event)))
 
-(defn GetGame-handleRequest [this is os context]
-  (handleRequest this is os context handle-get-game-event))
+(deflambda get-game)
 
 ;; Get Games
-
-(gen-class
-  :name GetGames
-  :prefix GetGames-
-  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
 
 (defn handle-get-games-event [event]
   (prn event)
   (game-store/get-games (:state event)))
 
-(defn GetGames-handleRequest [this is os context]
-  (handleRequest this is os context handle-get-games-event))
+(deflambda get-games)
 
 ;; Join Game
-
-(gen-class
-  :name JoinGame
-  :prefix JoinGame-
-  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
 
 (defn handle-join-game-event [{:keys [game-id email] :as event}]
   (prn event)
   (game-store/join-game game-id email))
 
-(defn JoinGame-handleRequest [this is os context]
-  (handleRequest this is os context handle-join-game-event))
+(deflambda join-game)
 
 ;; Make Move
-
-(gen-class
-  :name MakeMove
-  :prefix MakeMove-
-  :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
 
 (defn handle-make-move-event [{:keys [game-id email move] :as event}]
   (prn event)
   (game-store/make-move game-id email move))
 
-(defn MakeMove-handleRequest [this is os context]
-  (handleRequest this is os context handle-make-move-event))
+(deflambda make-move)
