18ff2080253100f9bd318834a39fa1a3433b3c4e
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2015-10-08 20:25:46 +0200

    Handling race condition in join-game.

--- serverless-rps-clj/src/serverless_rps_clj/game_store.clj
+++ serverless-rps-clj/src/serverless_rps_clj/game_store.clj
@@ -19,7 +19,9 @@
     {:gameId  game-id
      :state   :created
      :player1 player1})
-  {:gameId game-id})
+  {:gameId game-id
+   :email player1
+   :state :created})
 
 (defn get-game [game-id]
   (update-winner
@@ -35,21 +37,28 @@
     (map update-winner)))
 
 (defn join-game [game-id player2]
-  (update-winner (far/update-item *client-opts*
-                   :rpslab-games
-                   {:gameId game-id}
-                   {:player2 [:put player2]
-                    :state   [:put :ready]}
-                   {:return   :all-new
-                    :expected {:state [:eq :created]}})))
+  (let [current-game (get-game game-id)]
+    (if (not= "created" (:state current-game))
+      (if (= player2 (:player2 current-game))
+        current-game
+        (throw (Exception. "Game already started. Could not join game.")))
+      (update-winner (far/update-item *client-opts*
+                       :rpslab-games
+                       {:gameId game-id}
+                       {:player2 [:put player2]
+                        :state   [:put :ready]}
+                       {:return   :all-new
+                        :expected {:state [:eq :created]}})))))
 
 (defn- decide-state-field [player current-game]
   (let [{:keys [player1 player2 player1Move player2Move]} current-game]
     (condp = player
           player1 (cond (and (nil? player1Move) (nil? player2Move)) [:waiting :player1Move]
-                    player2Move [:ended :player1Move]) 
+                        player1Move [:waiting :player1Move]
+                        player2Move [:ended :player1Move])
           player2 (cond (and (nil? player1Move) (nil? player2Move)) [:waiting :player2Move]
-                    player1Move [:ended :player2Move]))))
+                        player2Move [:waiting :player2Move]
+                        player1Move [:ended :player2Move]))))
 
 (defn make-move [game-id player move]
   (let [current-game (get-game game-id)]
